8a19755a26c38101fdae752d02a1da197ba4f7a3
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-12-20 18:06:20 +0100

    Update he_IL, thanks to epifeny

---
+++ language/he_IL/plugin.lang.php
@@ -0,0 +1,15 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['%d comment on album'] = '%d תגובה על אלבום';
+$lang['%d comments on albums'] = '%d תגובות על אלבומים';
+$lang['Comments on albums'] = 'תגובות על אלבומים';
+$lang['Comments on photos'] = 'תגובות על תמונות';
+$lang['Display comments on'] = 'הצג תגובות על';
+$lang['Display last posted comments on albums'] = 'הצג את התגובות האחרונות על אלבומים';
+$lang['Last comments on albums'] = 'התגובות האחרונות על אלבומים';
+$lang['Photos'] = 'תמונות';
