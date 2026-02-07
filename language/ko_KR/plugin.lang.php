19a00b5ac2e5827e0ee8c6429f553ee891f5c2cd
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-02-07 19:16:07 +0100

    Update ko_KR, thanks to yjchoi

---
+++ language/ko_KR/plugin.lang.php
@@ -0,0 +1,15 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['%d comment on album'] = '앨범에 대한 %d개의 댓글';
+$lang['%d comments on albums'] = '앨범에 대한 %d개의 댓글';
+$lang['Comments on albums'] = '앨범에 대한 댓글';
+$lang['Comments on photos'] = '사진에 대한 댓글';
+$lang['Display comments on'] = '앨범에 대한 댓글 표시';
+$lang['Display last posted comments on albums'] = '앨범에 최근 게시된 댓글 표시';
+$lang['Last comments on albums'] = '앨범의 최근 댓글';
+$lang['Photos'] = '사진';
